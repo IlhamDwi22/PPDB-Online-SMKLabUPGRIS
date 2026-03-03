a9ff8fecefdaef2591f7357f71c4ed5102ff2e33
Mas'ul <masul.fauzi@gmail.com>
2026-03-03 14:53:44 +0700

    feat: login dulu pake breeze, ubah tampilan login

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string('id', 36)->primary();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('identitas', 30)->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
+            $table->string('created_by', 36)->nullable();
+            $table->string('updated_by', 36)->nullable();
+            $table->string('deleted_by', 36)->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
